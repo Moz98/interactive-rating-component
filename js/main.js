cb71cbc71cf90a7391297f4fdb7242784f7201ca
Moz98 <marcosoliveirazacarias@gmail.com>
2022-09-01 08:08:28 -0300

    Get rate - Js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const rates = Array.from(document.querySelectorAll('[data-rate]'));
+
+const rateMensage = document.querySelector('[data-rate-mensage]')
+
+
+function displayRate() {    
+    rateMensage.innerText = `You selected ${this.getAttribute('value')} out of ${rates.length}`;
+}
+
+rates.forEach(rate => rate.addEventListener(
+    'click',
+    displayRate
+))
+
