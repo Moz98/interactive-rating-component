ce44859da2fc5c5b1468d0feab540b5074873770
Moz98 <marcosoliveirazacarias@gmail.com>
2022-10-25 09:02:04 -0300

    Initial Js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,42 @@
-const rates = Array.from(document.querySelectorAll('[data-rate]'));
-const rateMensage = document.querySelector('[data-rate-mensage]')
+const button = document.querySelector("[data-button]");
+const cardTitle = document.querySelector("[data-card-title]");
 
+const hideElements = () => {
+  const elementsToHide = Array.from(
+    document.querySelectorAll("[data-element-hide]")
+  );
+  elementsToHide.forEach((element) => {
+    element.classList.add("hide");
+  });
+};
 
-function displayRate() {    
-    rateMensage.innerText = `You selected ${this.getAttribute('value')} out of ${rates.length}`;
-}
+const changeElements = () => {
+  cardTitle.innerHTML = "Thank you!";
 
-rates.forEach(rate => rate.addEventListener(
-    'click',
-    displayRate
-))
+  const cardDesc = document.querySelector("[data-card-desc]");
+  cardDesc.innerHTML =
+    "We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!";
+};
 
+const insertRatio = () => {
+  const createDiv = document.createElement("div");
+
+  const stars = 13;
+
+  const newContent = document.createTextNode(`Você escolheu ${stars} estrelas`);
+
+  const newDiv = createDiv.appendChild(newContent);
+
+  cardTitle.parentNode.insertBefore(newDiv, cardTitle);
+};
+
+const displayRatio = (e) => {
+  e.preventDefault();
+  button.innerHTML = "Submiting...";
+
+  //   hideElements();
+  //   changeElements();
+  insertRatio();
+};
+
+button.addEventListener("click", displayRatio);
